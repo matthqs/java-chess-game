27a810e468bb4234ac9964a22e67fa8a01fa69e2
matt <mathhqs@gmail.com>
2024-04-01 16:41:10 -0300

    feature/implement capture mechanism for bishop

--- src/piece/Bishop.java
+++ src/piece/Bishop.java
@@ -13,4 +13,15 @@ public class Bishop extends Piece{
             image = getImage("/piece/b_bishop");
         }
     }
+
+    public boolean canMove(int targetCol, int targetRow){
+        if(isWithinBoard(targetCol, targetRow) && !isSameSquare(targetCol, targetRow)){
+            if (Math.abs(targetCol - preCol) == Math.abs(targetRow - preRow)){
+                if(isValidSquare(targetCol, targetRow) && !isPieceOnDiagonalLine(targetCol, targetRow)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
