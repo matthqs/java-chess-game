daac893f09e441d995bb34f413622fb39b21d35f
matheus <mathhqs@gmail.com>
2024-04-04 00:01:31 -0300

    feature/implement Castling move

--- src/piece/King.java
+++ src/piece/King.java
@@ -16,9 +16,48 @@ public class King extends Piece{
 
     public boolean canMove(int targetCol, int targetRow){
 
+
         if(isWithinBoard(targetCol,targetRow) && isValidSquare(targetCol, targetRow)){
-            return Math.abs(targetCol - preCol) + Math.abs(targetRow - preRow) == 1 ||
-                    Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 1;
+            // Default movement
+
+            if(Math.abs(targetCol - preCol) + Math.abs(targetRow - preRow) == 1 ||
+                    Math.abs(targetCol - preCol) * Math.abs(targetRow - preRow) == 1){
+                if(isValidSquare(targetCol, targetRow)){
+                    return true;
+                }
+            }
+
+            // Castling
+            if(!moved) {
+                // Right castling
+                if(targetCol == preCol + 2 && targetRow == preRow && !pieceIsOnStraightLine(targetCol, targetRow)){
+                    for(Piece piece : GamePanel.simPieces) {
+                        if(piece.col == preCol+3 && piece.row == preRow && !piece.moved){
+                            GamePanel.castlingP = piece;
+                            return true;
+                        }
+                    }
+                }
+
+                // left castling
+                if(targetCol == preCol - 2 && targetRow == preRow && !pieceIsOnStraightLine(targetCol, targetRow)){
+                    Piece p[] = new Piece[2];
+                    for(Piece piece : GamePanel.simPieces) {
+                        if(piece.col == preCol - 3 && piece.row == targetRow){
+                            p[0] = piece;
+                        }
+                        if(piece.col == preCol - 4 && piece.row == targetRow){
+                            p[1] = piece;
+                        }
+
+                        if(p[0] == null && p[1] != null && !p[1].moved){
+                            GamePanel.castlingP = p[1];
+                            return true;
+                        }
+
+                    }
+                }
+            }
         }
 
         return false;
